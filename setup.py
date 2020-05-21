ab4b1c96b6b84da2e740bae9c4db7ecfe3905808
Rob DeJaco <dejac001@umn.edu>
2020-05-21 11:57:33 -0400

    Update setup.py and new directory structure

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="chem_util",
-    version="v0.0.2",
+    version="0.0.3",
     author="Robert F. De Jaco",
     author_email="dejac001@umn.edu",
     description="Random Chemistry/Chemical Engineering Utilities",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dejac001/chem_util",
-    packages=['src.util'],
+    packages=['chem_util'],
     python_requires='>=3.6',
+    package_dir={'': 'src'},
 )
